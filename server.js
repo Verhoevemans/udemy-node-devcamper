f85fbc24106ba28dc4515c25ebbbd63717540445
Jeroen Verhoeven <verhoevemans@gmail.com>
2022-11-06 23:01:16 +0100

    Small changes for new commit

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`));
+app.listen(PORT, () => {
+    console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`);
+});
